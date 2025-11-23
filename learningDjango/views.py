a259592cd4e3bd583b59b06fc9403e236d32a357
Chitraksh Suri <chitrakshsuri2005@gmail.com>
2025-11-24 02:25:10 +0530

    Add .gitignore and clean tracked cache files

--- learningDjango/views.py
+++ learningDjango/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("This is home page.")
+    # return HttpResponse("This is home page.")
+    return render(request, 'website/index.html')
 
 def about(request):
     return HttpResponse("This is about page.")
