a51d0921f14d96008c5b882aa0a3ee9189a3f807
Chitraksh Suri <chitrakshsuri2005@gmail.com>
2025-11-24 10:45:03 +0530

    Add home, about and contact templates and update views and styling

--- learningDjango/views.py
+++ learningDjango/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     # return HttpResponse("This is home page.")
-    return render(request, 'website/index.html')
+    return render(request, 'website/home.html')
 
 def about(request):
-    return HttpResponse("This is about page.")
+    # return HttpResponse("This is about page.")
+    return render(request, 'website/about.html')
 
 def contact(request):
-    return HttpResponse("This is contact page.")
+    # return HttpResponse("This is contact page.")
+    return render(request, 'website/contact.html')
